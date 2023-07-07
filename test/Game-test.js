f725770e22ab4177407313f6e444e204cc9aa436
Renee Pinna <renee.pinna@gmail.com>
2023-07-06 19:50:32 -0600

    Add small sample set for testing initiateDeck to test suite

--- test/Game-test.js
+++ test/Game-test.js
@@ -1,15 +1,17 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const {printMessage, printQuestion, start, initiateDeck} = require('../src/game');
-const data = require('../src/data');
-const prototypeQuestions = data.prototypeData;
+const {createCard} = require('../src/card')
+const {initiateDeck} = require('../src/game');
 
 describe('Game Start', function() {
   it('should create a deck', function() {
-    const deck = initiateDeck(prototypeQuestions);
+    card1 = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    card2 = createCard(2, "What is a comma-separated list of related values?", ["array", "object", "function"],"array")
+    card3 = createCard(3, "What type of prototype method directly modifies the existing array?", ["mutator method", "accessor method", "iteration method"], "mutator method")
 
-    expect(deck).to.deep.equal(prototypeQuestions);
-    
+    const deck = initiateDeck([card1, card2, card3]);
+
+    expect(deck).to.deep.equal([card1, card2, card3]);
   })
 })
