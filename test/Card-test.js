ad9fe63fbba6614fe0daee3a7f0c1041f4db3228
Renee Pinna <renee.pinna@gmail.com>
2023-07-06 19:39:25 -0600

    Remove/Add whitespace

--- test/Card-test.js
+++ test/Card-test.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const { createCard, createDeck, countCards} = require('../src/card');
+const { createCard, createDeck, countCards } = require('../src/card');
 
 describe('card', function() {
   it('should be a function', function() {
